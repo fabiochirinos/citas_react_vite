47160f89f13f7e15957202709eb7c5aee5874a80
Fabio <fabiochirinos7@gmail.com>
2023-01-18 18:14:41 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 📌 Citas con React y Vite
+¡Hola! Esta es una aplicación desarrollada con React, una página de citas interactiva que realiza CRUD de datos que se guardan en el LocalStorage. 
+
+## Ejecutar la Aplicación
+Para iniciar la aplicación, debes ejecutar el comando `npm run dev` en el terminal. Si trabajas con Visual Studio Code, puedes abrir el terminal con el atajo de teclado `ctrl + ñ` si tu teclado está en español y con ``ctrl + ` `` si tu teclado está en inglés.
+
+## Instalar Módulos
+Para instalar los módulos necesarios para la aplicación, debes ejecutar el comando `npm install` en el terminal.
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react"
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+function App() {
+
+  const [pacientes, setPacientes] = useState(() => JSON.parse(localStorage.getItem('pacientes')) || []);
+  const [paciente, setPaciente] = useState({})
+
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  }, [pacientes])
+
+
+  //Eliminando paciente
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+
+    setPacientes(pacientesActualizado<s)
+  }
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
